459f34a853e0ddfb6417e2b0d0785225168b35b8
triinoxys <contact@triinoxys.fr>
2019-04-02 13:59:23 +0200

    day06/ex00

--- day06/ex00/Color.class.php
+++ day06/ex00/Color.class.php
@@ -7,39 +7,40 @@ class Color
 	public $red = 0;
 	public $green = 0;
 	public $blue = 0;
+	public $dec = 0;
 
 	function __construct($params)
 	{
 		if (!empty($params['rgb']))
 		{
-			list($this->red, $this->green, $this->blue) = sscanf($params['rgb'], "#%02x%02x%02x");
-		
+			$this->red   = floor($params['rgb'] / (256 * 256));
+			$this->green = floor($params['rgb'] / 256) % 256;
+			$this->blue  = $params['rgb'] % 256;
 		}
 		else if (isset($params['red']) && isset($params['green']) && isset($params['blue']))
 		{
 			$this->red   = floor($params['red'  ]);
 			$this->green = floor($params['green']);
 			$this->blue  = floor($params['blue' ]);
-			if (self::$verbose === True)
-				echo "Color( red: ", str_pad($this->red, 3, ' ', STR_PAD_LEFT), ", green: ", str_pad($this->green, 3, ' ', STR_PAD_LEFT), ", blue: ", str_pad($this->blue, 3, ' ', STR_PAD_LEFT), " ) constructed.\n";
 		}
-		else exit("ERROR\n");
+		if (self::$verbose === True)
+			echo "Color( red: ", str_pad($this->red, 3, ' ', STR_PAD_LEFT), ", green: ", str_pad($this->green, 3, ' ', STR_PAD_LEFT), ", blue: ", str_pad($this->blue, 3, ' ', STR_PAD_LEFT), " ) constructed.\n";
 	}
 
 	function __destruct()
 	{
-		//if (self::$verbose === True)
-		//	echo "Destruct", "\n";
+		if (self::$verbose === True)
+			echo "Color( red: ", str_pad($this->red, 3, ' ', STR_PAD_LEFT), ", green: ", str_pad($this->green, 3, ' ', STR_PAD_LEFT), ", blue: ", str_pad($this->blue, 3, ' ', STR_PAD_LEFT), " ) destructed.\n";
 	}
 
 	function __toString()
 	{
-		return "Color( red: $this->red, green: $this->green, blue: $this->blue )\n";
+		return ("Color( red: " . str_pad($this->red, 3, ' ', STR_PAD_LEFT) . ", green: " . str_pad($this->green, 3, ' ', STR_PAD_LEFT) . ", blue: " . str_pad($this->blue, 3, ' ', STR_PAD_LEFT) . " )");
 	}
 
 	public function doc()
 	{
-		return "lol une doc\n";
+		return "\n" . file_get_contents('Color.doc.txt');
 	}
 
 	public function add($other)
@@ -52,9 +53,9 @@ class Color
 		return new Color(array('red' => $this->red - $other->red, 'green' => $this->green - $other->green, 'blue' => $this->blue - $other->blue));
 	}
 
-	public function mult($other)
+	public function mult($f)
 	{
-		return new Color(array('red' => $this->red * $other->red, 'green' => $this->green * $other->green, 'blue' => $this->blue * $other->blue));
+		return new Color(array('red' => $this->red * $f, 'green' => $this->green * $f, 'blue' => $this->blue * $f));
 	}
 }
 
